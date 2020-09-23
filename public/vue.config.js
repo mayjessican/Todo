5b811e6b6e4dad6ec5c0df3a6716cf0b9b6fef29
mayjessican <63651624+mayjessican@users.noreply.github.com>
2020-09-23 13:33:52 +0200

    task 3.9 todo list

---
+++ public/vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: '/To-Do-VueJS'
+}
---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { MdField } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import VueMaterial from 'vue-material';
+
+Vue.use(MdField);
+Vue.use(VueMaterial);
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
